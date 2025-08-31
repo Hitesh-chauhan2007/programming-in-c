e3607643fadf4b689ddc219003a28953489c8ada
Hitesh-chauhan2007 <whoo.knee@gmail.com>
2025-08-31 23:19:30 +0530

    Create Experiment 1.1.c

---
+++ Experiment 1/Experiment 1.1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main ()
+{
+ printf("Name - Hitesh\nSAP_ID-590027742\nCourse - BCA\nBatch - 6");
+
+ printf("\n-----------------------------------\n");
+
+ printf("HelloWorld");
+
+ return 0;
+
+}
